39ee99c1f6ae3c14863019c4e0395f901f48688e
AJPascoe <pascoe312@gmail.com>
2021-09-06 14:19:13 +0100

    added 2 varibles favActor and director

--- src/app.js
+++ src/app.js
@@ -2,5 +2,7 @@ const {goodOrBad}= require("./utils");
 const likeVar = process.argv[2];
 const movie = process.argv[3];
 const actor = process.argv[4]
+const director = process.argv[5];
+const favActor = process.argv[6];
 
-goodOrBad(likeVar, movie, actor);
+goodOrBad(likeVar, movie, actor, director, favActor);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,20 +1,36 @@
-exports.goodOrBad = (likeVar, movie, actor) => {
-  if (actor) {
-     if (likeVar === "good") {
-    console.log(`I think ${movie} is good with ${actor}`);
-  } else if (likeVar === "bad") {
-    console.log(`I think ${movie} is bad with ${actor}`);
-  } else {
-    console.log("Incorrect input");
-  }
-} else {
-    if (likeVar === "good") {
-        console.log(`I think ${movie} is good`);
-      } else if (likeVar === "bad") {
-        console.log(`I think ${movie} is bad`);
-      } else {
-        console.log("Incorrect input");
+exports.goodOrBad = (likeVar, movie, actor, director, favActor) => {
+  if (favActor) {
+    if (likeVar === "fav") {
+      console.log(`My favourite character in ${movie} is ${favActor}`);
+    } else if(likeVar==="none"){
+      console.log("No favourite Actor");
     }
-}
-}
+  } else if (director) {
+    if (likeVar === "good") {
+      console.log(
+        `I think the director ${director} did a good job of ${movie}`
+      );
+    } else if (likeVar === "bad") {
+      console.log(`I think ${director} did a bad job of ${movie}`);
+    } else {
+      console.log("Incorrect input");
+    }
+  } else if (actor) {
+    if (likeVar === "good") {
+      console.log(`I think ${movie} is good with ${actor}`);
+    } else if (likeVar === "bad") {
+      console.log(`I think ${movie} is bad with ${actor}`);
+    } else {
+      console.log("Incorrect input");
+    }
+  } else {
+    if (likeVar === "good") {
+      console.log(`I think ${movie} is good`);
+    } else if (likeVar === "bad") {
+      console.log(`I think ${movie} is bad`);
+    } else {
+      console.log("Incorrect input");
+    }
+  }
+};
 // module.exports = goodOrBad
