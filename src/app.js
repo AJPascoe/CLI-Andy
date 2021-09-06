67afc3439c71df739c1092f0e1d02d33ec4e9103
AJPascoe <pascoe312@gmail.com>
2021-09-06 12:18:07 +0100

    adding actor

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const {goodOrBad}= require("./utils");
 const likeVar = process.argv[2];
 const movie = process.argv[3];
+const actor = process.argv[4]
 
-goodOrBad(likeVar, movie)
+goodOrBad(likeVar, movie, actor);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,20 @@
-exports.goodOrBad = (likeVar, movie) => {
-  if (likeVar === "good") {
-    console.log(`I think ${movie} is good`);
+exports.goodOrBad = (likeVar, movie, actor) => {
+  if (actor) {
+     if (likeVar === "good") {
+    console.log(`I think ${movie} is good with ${actor}`);
   } else if (likeVar === "bad") {
-    console.log(`I think ${movie} is bad`);
+    console.log(`I think ${movie} is bad with ${actor}`);
   } else {
     console.log("Incorrect input");
   }
-};
-
+} else {
+    if (likeVar === "good") {
+        console.log(`I think ${movie} is good`);
+      } else if (likeVar === "bad") {
+        console.log(`I think ${movie} is bad`);
+      } else {
+        console.log("Incorrect input");
+    }
+}
+}
 // module.exports = goodOrBad
