4a5f1c2671078667a1593024b655ba2bb438b8c9
AJPascoe <pascoe312@gmail.com>
2021-09-07 11:33:32 +0100

    adding read and write filesyne

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,20 @@
 const fs = require("fs");
 
+const addMovie = (likeVar, movie, actor, director, favActor) => {
+  const lessStringyList = JSON.parse(fs.readFileSync("./storage.json"));
+  const updatedObj = [
+    lessStringyList.flat(),
+    { name: movie, like: likeVar, actor: actor, favActor: favActor, director: director},
+  ];
+  fs.writeFileSync("./storage.json", JSON.stringify(updatedObj));
+};
 
-
-exports.goodOrBad = (likeVar, movie, actor, director, favActor) => {
+exports.goodOrBad = (likeVar, movie, actor, favActor, director) => {
   if (favActor) {
     if (likeVar === "fav") {
       console.log(`My favourite character in ${movie} is ${favActor}`);
-    } else if(likeVar==="none"){
+
+    } else if (likeVar === "none") {
       console.log("No favourite Actor");
     }
   } else if (director) {
@@ -14,6 +22,7 @@ exports.goodOrBad = (likeVar, movie, actor, director, favActor) => {
       console.log(
         `I think the director ${director} did a good job of ${movie}`
       );
+
     } else if (likeVar === "bad") {
       console.log(`I think ${director} did a bad job of ${movie}`);
     } else {
@@ -22,26 +31,27 @@ exports.goodOrBad = (likeVar, movie, actor, director, favActor) => {
   } else if (actor) {
     if (likeVar === "good") {
       // console.log(`I think ${movie} is good with ${actor}`);
-        const stringyObj = JSON.stringify({
-        name: movie,
-        like: likeVar,
-        actor: actor
-      });
-      fs.writeFileSync("../../storage.json", stringyObj)
-
-    } else if (likeVar === "bad") {
-      console.log(`I think ${movie} is bad with ${actor}`);
-    } else {
-      console.log("Incorrect input");
+    
     }
-  } else {
-    if (likeVar === "good") {
-      console.log(`I think ${movie} is good`);
-    } else if (likeVar === "bad") {
-      console.log(`I think ${movie} is bad`);
+
+    if (actor) {
+      if (likeVar === "good") {
+        // console.log(`I think ${movie} is good with ${actor}`);
+
+      } else if (likeVar === "bad") {
+        console.log(`I think ${movie} is bad with ${actor}`);
+      } else {
+        console.log("Incorrect input");
+      }
     } else {
-      console.log("Incorrect input");
+      if (likeVar === "good") {
+        console.log(`I think ${movie} is good`);
+      } else if (likeVar === "bad") {
+        console.log(`I think ${movie} is bad`);
+      } else {
+        console.log("Incorrect input");
+      }
     }
   }
+  addMovie(likeVar, movie, actor, director, favActor )
 };
-// module.exports = goodOrBad
